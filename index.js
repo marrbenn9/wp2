2d7111ab120e59a8330626673d5c5f1422913cdf
RJ Moon <marrbenn9@gmail.com>
2025-07-18 21:24:34 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const User = require('./models/User')
+
+
+async function mong_connect(){
+    try {
+        let x = await mongoose.connect('mongodb+srv://marrbenn1:reuben123@cluster1.kx7dslo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1')
+        console.log('Mongoose Success!')
+        
+    } catch (err) {
+        console.log(`Mongo Error: ${err}`)
+    }
+    
+   
+}
+
+mong_connect()
+
+app.listen(4000, () => {
+    console.log("Server running on port 4000")
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello from backend!')
+})
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find(); // Fetch all users
+    res.json(users); // Send as JSON
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+});
+
